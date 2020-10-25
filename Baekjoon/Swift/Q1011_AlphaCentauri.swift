b2ae1e6a39a774a44ee5a1d9aa6864c53082bcad
thoonk <dkrmrm878@gmail.com>
2020-10-25 16:07:44 +0900

    Add Q1011_AlphaCentauri

---
+++ Baekjoon/Swift/Q1011_AlphaCentauri.swift
@@ -0,0 +1,36 @@
+//
+//  main.swift
+//  BOJ_PS
+//
+//  Created by 김태훈 on 2020/10/24.
+//
+
+import Foundation
+
+var T = Int(readLine()!)!
+
+for _ in 0 ..< T{
+    let nums = readLine()!.split(separator: " ").map { Int($0)!}
+    
+    let distance = nums[1] - nums[0]
+    let result = computeDistance(distance)
+    print(result)
+}
+
+
+func computeDistance (_ d: Int) -> Int{
+    
+    var cnt = 0
+    let max = Int(sqrt(Double(d)))
+    let powMax: Int = max * max
+    
+    if d == powMax {
+        cnt = 2 * max - 1
+    } else if powMax < d, d <= powMax + max{
+        cnt = 2 * max
+    } else if powMax + max < d, d < (max + 1) * (max + 1) {
+        cnt = 2 * max + 1
+    }
+    
+    return cnt
+}
