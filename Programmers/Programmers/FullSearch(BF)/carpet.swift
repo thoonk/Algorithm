7541cca5b774ef7a17da2c6b855fb3ab54895513
thoonk <dkrmrm878@gmail.com>
2020-11-28 19:39:11 +0900

    Add remark

--- Programmers/Programmers/FullSearch(BF)/carpet.swift
+++ Programmers/Programmers/FullSearch(BF)/carpet.swift
@@ -15,10 +15,12 @@ func solution(_ brown:Int, _ yellow:Int) -> [Int] {
     var col = 0
     
     for i in stride(from: 1, through: total, by: 1) {
+        // 약수 구하기
         if total % i == 0{
             row = total / i
             col = i
         }
+        // 규칙을 이용해서 row, col 구하기
         if (row - 2) * (col - 2) == yellow {
             result.append(row)
             result.append(col)
