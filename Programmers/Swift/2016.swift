bfdb448fd788c1e3d2d20f8582ef9e68731b8089
thoonk <dkrmrm878@gmail.com>
2020-12-28 18:48:17 +0900

    Add 2016

---
+++ Programmers/Swift/2016.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 김태훈 on 2020/12/28.
+//
+
+import Foundation
+
+func solution(_ a:Int, _ b:Int) -> String {
+    
+    let week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+    let month = [31,29,31,30,31,30,31,31,30,31,30,31]
+    
+    var day = month[0..<a-1].reduce(0,+) + b+4
+    
+    return week[day%7]
+}
+
+print(solution(5, 24))
