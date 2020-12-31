60084041f0f416f030ec7ec02209ab292df20ad4
thoonk <dkrmrm878@gmail.com>
2020-12-31 22:37:52 +0900

    Add sumBtwTwoNum

---
+++ Programmers/Swift/sumBtwTwoNum.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 김태훈 on 2020/12/29.
+//  두 정수 사이의 합
+
+import Foundation
+
+func solution(_ a:Int, _ b:Int) -> Int64 {
+    
+    var result: Int64 = 0
+    
+    if a == b {
+        return Int64(a)
+    } else if a < b {
+        for i in stride(from: a, through: b, by: 1) {
+            result += Int64(i)
+        }
+    } else {
+        for i in stride(from: b, through: a, by: 1) {
+            result += Int64(i)
+        }
+    }
+    return result
+}
