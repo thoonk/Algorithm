cbd5340a3b3fef5e82202201e90dc226bbca5beb
thoonk <dkrmrm878@gmail.com>
2021-01-01 21:52:37 +0900

    Add flipTernary

---
+++ Programmers/Swift/flipTernary.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 김태훈 on 2021/01/01.
+//  3진법 뒤집기
+
+import Foundation
+
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    return Int(String(String(n, radix: 3).reversed()), radix: 3)!
+}
+
+print(solution(45))
