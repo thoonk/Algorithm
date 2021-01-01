765a700151f24a5f9310e3eef7c8256543e58025
thoonk <dkrmrm878@gmail.com>
2021-01-01 21:52:46 +0900

    Add handleString

---
+++ Programmers/Swift/handleString.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 김태훈 on 2021/01/01.
+//  문자열 다루기 기본
+
+import Foundation
+
+func solution(_ s:String) -> Bool {
+    
+    var ans = true
+    if s.count == 4 || s.count == 6 {
+        for c in s {
+            if Int(String(c)) == nil {
+                ans = false
+            }
+        }
+    } else {
+        ans = false
+    }
+    return ans
+}
