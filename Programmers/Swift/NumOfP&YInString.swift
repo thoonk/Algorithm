eac5c8515207aad116a14ca07198e2b5e8919730
thoonk <dkrmrm878@gmail.com>
2021-01-01 21:52:59 +0900

    Add NumOfP&YInString

---
+++ Programmers/Swift/NumOfP&YInString.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 김태훈 on 2021/01/01.
+//  문자열 내 p와 y의 개수
+
+import Foundation
+
+func solution(_ s:String) -> Bool
+{
+    let s = s.lowercased()
+    let pCnt = s.filter { $0 == "p" }.count
+    let yCnt = s.filter { $0 == "y" }.count
+
+    return pCnt == yCnt ? true : false
+}
+
+print(solution("pPoooyY"))
