75af0f5942ae48ed59f41825792ab8c42277800b
thoonk <dkrmrm878@gmail.com>
2021-01-03 00:27:12 +0900

    Add replaceStringWithint

---
+++ Programmers/Swift/replaceStringWithInt.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 김태훈 on 2021/01/02.
+//  문자열을 정수로 바꾸기 Level1
+
+import Foundation
+
+func solution(_ s:String) -> Int {
+    return Int(s)!
+}
+
+print(solution("-1234"))
