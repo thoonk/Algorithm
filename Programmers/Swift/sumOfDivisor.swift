32bc912046fa78b0e1a34fcaa9aa1900f3529a85
thoonk <dkrmrm878@gmail.com>
2021-01-05 00:11:01 +0900

    Add sumOfDivisor

---
+++ Programmers/Swift/sumOfDivisor.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 김태훈 on 2021/01/04.
+//  약수의 합
+
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    
+    var result = Set<Int>()
+    
+    if n == 0 {
+        return 0
+    } else {
+        for i in 1...n {
+            if n%i == 0 {
+                result.insert(i)
+            }
+        }
+    }
+    
+    return result.reduce(0,+)
+}
+
+print(solution(12))
