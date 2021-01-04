20635b1ee8df17d690d0e78d0cadf0e6585a9b50
thoonk <dkrmrm878@gmail.com>
2021-01-05 00:10:48 +0900

    Add dotProduct

---
+++ Programmers/Swift/dotProduct.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 김태훈 on 2021/01/04.
+//  내적
+
+import Foundation
+
+func solution(_ a:[Int], _ b:[Int]) -> Int {
+    
+    var result = 0
+    
+    for i in 0..<a.count {
+        result += a[i] * b[i]
+    }
+    
+    return result
+}
