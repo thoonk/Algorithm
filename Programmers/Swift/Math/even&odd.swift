276017403bbe370d376cbbabeec37b6a71f33a28
thoonk <dkrmrm878@gmail.com>
2021-01-06 00:35:53 +0900

    Add even&odd

---
+++ Programmers/Swift/Math/even&odd.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Programmers
+//
+//  Created by 김태훈 on 2021/01/06.
+//  짝수와 홀수 Level1
+
+import Foundation
+
+func solution(_ num:Int) -> String {
+    return num % 2 == 0 ? "Even" : "Odd"
+}
