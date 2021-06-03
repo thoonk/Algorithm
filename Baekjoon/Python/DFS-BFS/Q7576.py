96609b389e5d125482ed7cfc001a25bee8afa191
thoonk <dkrmrm878@gmail.com>
2021-06-03 17:06:42 +0900

    [Add] Q7576

---
+++ Baekjoon/Python/DFS-BFS/Q7576.py
@@ -0,0 +1,43 @@
+# 토마토 / BFS
+import sys
+from collections import deque
+input = sys.stdin.readline
+m, n = map(int, input().split())
+box = [list(map(int, input().split())) for _ in range(n)]
+ripen = 0
+zero = 0
+q = deque([])
+res = 0
+
+def bfs():
+    global ripen, res
+    d = [(-1,0),(1,0),(0,-1),(0,1)]
+    while q:
+        r, c = q.popleft()
+
+        for i in range(4):
+            nr = r + d[i][0]
+            nc = c + d[i][1]
+
+            if 0 <= nr < n and 0 <= nc < m and box[nr][nc] == 0:
+                q.append((nr,nc))
+                box[nr][nc] = box[r][c] + 1
+                ripen += 1 
+                res = max(res, box[nr][nc])
+
+def solution():
+    global zero
+    for r in range(n):
+        for c in range(m):
+            if box[r][c] == 1 :
+                q.append((r,c))
+            elif box[r][c] == 0:
+                zero += 1
+
+    bfs()
+    if zero == 0:
+        print(0)
+    else:
+        print(res-1 if zero == ripen else -1)
+
+solution()
--- Baekjoon/Swift/DFS-BFS/Q7576-tomato.swift
+++ Baekjoon/Swift/DFS-BFS/Q7576-tomato.swift
@@ -5,58 +5,65 @@
 //  Created by 김태훈 on 2020/12/20.
 //  토마토 7576 BFS
 
-import Foundation
+typealias Coord = (r: Int, c: Int)
 
-let mn = readLine()!.split(separator: " ").map {Int(String($0))!}
+let mn = readLine()!.split(separator: " ").map { Int(String($0))! }
 let m = mn[0]
 let n = mn[1]
 
-var map = [[Int]]()
-var result = 0
-var q = [(Int,Int)]()
-let d = [(0,-1),(0,1),(-1,0),(1,0)]
+var box = [[Int]]()
+var q = [Coord]()
+var ripen = 0
 var zero = 0
-var cnt = 0
-
-for i in 0 ..< n {
-    let row = readLine()!.split(separator: " ").map {Int(String($0))!}
-    map.append(row)
-    for j in 0 ..< m {
-        if map[i][j] == 1 {
-            q.append((i,j))
-        } else if map[i][j] == 0 {
-            zero += 1
-        }
-    }
-}
 
 func bfs() {
-    var idx = 0
-    while q.count > idx {
-        
-        let (x,y) = q[idx]
-        idx += 1
-        
+    let d: [Coord] = [(-1,0),(1,0),(0,-1),(0,1)]
+    
+    var index = 0
+    while index < q.count {
+        let now = q[index]
+        index += 1
         
         for i in 0 ..< 4 {
-            let nx = x + d[i].0
-            let ny = y + d[i].1
+            let nr = now.r + d[i].r
+            let nc = now.c + d[i].c
             
-            if 0<=nx, nx<n, 0<=ny, ny<m, map[nx][ny]==0 {
-                q.append((nx,ny))
-                map[nx][ny] = map[x][y]+1
-                result = max(result, map[nx][ny])
-                cnt += 1
+            guard 0 <= nr && nr < n && 0 <= nc && nc < m else { continue }
+            
+            // 익지 않았다면 날짜 갱신 및 익은 토마토 개수 증가
+            if box[nr][nc] == 0 {
+                q.append((nr, nc))
+                box[nr][nc] = box[now.r][now.c] + 1
+                ripen += 1
             }
         }
     }
 }
 
-bfs()
+func solution() {
+    for _ in 0 ..< n {
+        box.append(readLine()!.split(separator: " ").map { Int(String($0))! })
+    }
+    
+    for r in 0 ..< n {
+        for c in 0 ..< m {
+            if box[r][c] == 1 {
+                q.append((r,c))
+            } else if box[r][c] == 0 {
+                zero += 1
+            }
+        }
+    }
 
-if zero == 0 {
-    print(zero)
-} else {
-    print(zero == cnt ? result-1 : -1)
+    bfs()
+    // 모든 토마토가 익어 있는 상태
+    if zero == 0 {
+        print(0)
+    }
+    // 처음 익지 않은 상태와 익은 토마토의 개수가 같다면 익지 못하는 토마토가 없음
+    else {
+        print(zero == ripen ? box.flatMap({$0}).max()! - 1 : -1)
+    }
 }
 
+solution()
