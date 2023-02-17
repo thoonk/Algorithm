3a0f8cecafafae7756d1430e7c4daeb91f957386
thoonk <dkrmrm878@naver.com>
2023-02-17 22:09:54 +0900

    [Add] Q2178.py

--- Baekjoon/Python/BOJ_PS/DFS-BFS/Q2178.py
+++ Baekjoon/Python/BOJ_PS/DFS-BFS/Q2178.py
@@ -1,31 +1,25 @@
 # 미로 탐색 / BFS
 import sys
+from collections import deque
 input = sys.stdin.readline
-
 n, m = map(int, input().split())
-maze = []
-for i in range(n):
-    maze.append(list(map(int, input().strip())))
+maze = [list(map(int, input().rstrip())) for _ in range(n)]
 
-def solution():
-    d = [[-1,0],[1,0],[0,-1],[0,1]]
-    q = []
-    q.append([0, 0])
-    maze[0][0] = 2
+q = deque([(0, 0)])
+d = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
-    while q:
-        r, c = q.pop(0)
-        for i in range(4):
-            nr = r + d[i][0]
-            nc = c + d[i][1]
-            
-            if 0 <= nr < n and 0 <= nc < m:
-                if maze[nr][nc] == 1:
-                    q.append([nr, nc])
-                    maze[nr][nc] = maze[r][c] + 1
-solution()
-for i in range(n):
-    print(maze[i])
-print(maze[n-1][m-1]-1)
+while q:
+    r, c = q.popleft()
+    
+    for i in range(4):
+        nr = r + d[i][0]
+        nc = c + d[i][1]
+        
+        if 0 <= nr < n and 0 <= nc < m:
+            if maze[nr][nc] == 1:
+                q.append((nr, nc))
+                maze[nr][nc] += maze[r][c]
+
+print(maze[n - 1][m - 1])
 
 
