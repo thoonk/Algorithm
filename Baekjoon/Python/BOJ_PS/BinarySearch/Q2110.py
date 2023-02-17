3916fbdc7015e349914b6524005c8e53579665f3
thoonk <dkrmrm878@naver.com>
2023-02-17 22:09:31 +0900

    [Add] Q2110.py

--- Baekjoon/Python/BOJ_PS/BinarySearch/Q2110.py
+++ Baekjoon/Python/BOJ_PS/BinarySearch/Q2110.py
@@ -1,26 +1,29 @@
-# 공유기 설치 / 이분탐색
-n,c = map(int, input().split())
-homes = [int(input()) for _ in range(n)]
-homes.sort()
+# 공유기 설치 / 이진탐색
+import sys
+input = sys.stdin.readline
 
-start = 1
-end = homes[-1] - homes[0]
-result = 0
+n, c = map(int, input().split())
+houses = [int(input()) for _ in range(n)]
+houses.sort()
 
-while (start <= end):
-    mid = (start + end) // 2 
-    install = homes[0]
-    cnt = 1
+answer = 0
+start, end = 1, houses[-1] - houses[0]
 
-    for i in range(1, n):
-        if homes[i] >= install + mid:
-            cnt += 1
-            install = homes[i]
+
+while start <= end:
+    mid = (start + end) // 2
+    count = 1
+    installed = houses[0]
     
-    if cnt >=c:
+    for i in range(1, n):
+        if houses[i] >= installed + mid:
+            count += 1
+            installed = houses[i]
+        
+    if count >= c:
         start = mid + 1
-        result = mid
+        answer = mid
     else:
         end = mid - 1
-
-print(result)
+        
+print(answer)
