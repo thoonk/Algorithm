9f2f30b5239b8d30b2d20af97376d1a682cc6eb9
thoonk <dkrmrm878@gmail.com>
2024-02-01 23:07:10 +0900

    추가: 삼각 달팽이(68645)

--- Programmers/Swift/Implementation/triangleSnail.swift
+++ Programmers/Swift/Implementation/triangleSnail.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 func solution(_ n:Int) -> [Int] {
-    
     let d = [(1,0), (0,1), (-1,-1)]
     var direct = 0
     var row = 0, col = 0
@@ -36,4 +35,39 @@ func solution(_ n:Int) -> [Int] {
     
     return result.reduce(into: [Int]()) { $0.append(contentsOf: $1)}
 }
-print(solution(4))
+
+import Foundation
+
+func solution2(_ n:Int) -> [Int] {
+    let d = [(1,0), (0,1), (-1,-1)]
+    var direct = 0
+    var r = 0, c = 0
+    var num = 1
+    var result = [[Int]]()
+    let sum = (n+1) * n / 2
+    
+    (1...n).forEach {
+        result.append([Int](repeating: 0, count: $0))
+    }
+    
+    while num <= sum {
+        result[r][c] = num
+        let nr = r + d[direct].0
+        let nc = c + d[direct].1
+        
+        if 0 <= nr, nr < n, 0 <= nc, nc <= nr, result[nr][nc] == 0 {
+            r = nr
+            c = nc
+        } else {
+            direct = (direct+1) % 3
+            r += d[direct].0
+            c += d[direct].1
+        }
+        
+        num += 1
+    }
+    
+    return result.flatMap { $0 }
+}
+
+print(solution2(4))
